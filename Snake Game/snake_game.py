87f3f178a0923097e2e5411584111346d4b10563
DewaGitarist <160745121+DewaGitarist@users.noreply.github.com>
2024-07-22 12:23:00 +0200

    Update snake_game.py

--- Snake Game/snake_game.py
+++ Snake Game/snake_game.py
@@ -193,3 +193,21 @@ main
 /////////////
 /////////////
 ///////
+  </div>
+        <!--end of single store item-->
+        <!-- single item -->
+        <div class="col-10 col-sm-6 col-lg-4 mx-auto my-3 store-item doughnuts" data-item="dougnuts">
+          <div class="card ">
+            <div class="img-container">
+              <img src="img/doughnut-1.jpeg" class="card-img-top store-img" alt="">
+              <span class="store-item-icon">
+                <i class="fas fa-shopping-cart"></i>
+              </span>
+            </div>
+            <div class="card-body">
+              <div class="card-text d-flex justify-content-between text-capitalize">
+                <h5 id="store-item-name">dougnut item</h5>
+                <h5 class="store-item-value">$ <strong id="store-item-price" class="font-weight-bold">5</strong></h5>
+
+              </div>
+            </div>
