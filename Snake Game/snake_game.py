f4b6b5a64e9970a3787c9a81b92edd2fcb70f4b1
DewaGitarist <160745121+DewaGitarist@users.noreply.github.com>
2025-08-09 19:24:43 +0200

    Update snake_game.py

--- Snake Game/snake_game.py
+++ Snake Game/snake_game.py
@@ -10,7 +10,14 @@ SNAKE_COLOR = "#00FF00"
 FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#000000"
 
-
+GAME_WIDTH = 705
+GAME_HEIGHT = 700
+SPEED = 100
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
 class Snake:
 
     def __init__(self):
@@ -270,3 +277,4 @@ main
 ////////////////
 //////////////////
 /////////////
+
